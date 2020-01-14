81d1eb31bb95354050e9b2b0779a709ba32a5aca
mariofalvarez <mariofalvarezjr@gmail.com>
2020-01-13 19:03:01 -0800

    added process.env.PORT

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-const express = require("express");
-const app = express();
-const PORT = 8000;
+const express = require("express")
+const app = express()
+const PORT = process.env.PORT || 8000
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(express.static("./client"));
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(express.static("./client"))
 
-const apiRoutes = require("./routes/apiRoutes");
-app.use("/", apiRoutes);
+const apiRoutes = require("./routes/apiRoutes")
+app.use("/", apiRoutes)
 
 app.listen(PORT, () => {
-    console.log(`listening on local: http://localhost:${PORT}`);
-});
+  console.log(`listening on local: http://localhost:${PORT}`)
+})
