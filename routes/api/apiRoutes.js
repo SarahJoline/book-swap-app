15fffdb0ad7f567a1041f5580d4582b29c1883d8
Martin Nguyen <ngumartin@gmail.com>
2020-01-15 20:36:44 -0800

    changes to html/js path

--- routes/api/apiRoutes.js
+++ routes/api/apiRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const books = require("../models/books");
+const books = require("../../models/books");
 
 router.get("/", (req, res) => {
   books.findAll().then(allBooks => {
