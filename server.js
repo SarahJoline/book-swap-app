1d90ad70c80ab5b0827bc9e5cf0c57c1ef3f3826
mariofalvarez <mariofalvarezjr@gmail.com>
2020-01-15 20:31:03 -0800

    updated html routes

--- server.js
+++ server.js
@@ -1,18 +1,17 @@
-const express = require("express")
-const app = express()
-const PORT = process.env.PORT || 7000
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 7000;
 
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
-app.use(express.static("./client"))
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("./client"));
 
+const apiRoutes = require("./routes/api/apiRoutes");
+app.use("/", apiRoutes);
 
-const apiRoutes = require("./routes/api/apiRoutes")
-app.use("/", apiRoutes)
-
-const clientRoutes = require("./routes/client/clientRoutes")
-app.use("/", clientRoutes)
+const clientRoutes = require("./routes/client/clientRoutes");
+app.use("/", clientRoutes);
 
 app.listen(PORT, () => {
-  console.log(`listening on local: http://localhost:${PORT}`)
-})
+  console.log(`listening on local: http://localhost:${PORT}`);
+});
