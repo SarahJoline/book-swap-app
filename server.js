6fee3f9eb4e5733b4a27dca11ad2e9385e2c5933
mariofalvarez <mariofalvarezjr@gmail.com>
2020-01-15 18:25:25 -0800

    cleaned up some files and names

--- server.js
+++ server.js
@@ -9,8 +9,9 @@ app.use(express.static("./client"))
 const apiRoutes = require("./routes/api/apiRoutes")
 app.use("/", apiRoutes)
 
+const clientRoutes = require("./routes/client/clientRoutes")
+app.use("/", clientRoutes)
+
 app.listen(PORT, () => {
   console.log(`listening on local: http://localhost:${PORT}`)
 })
-
-console.log("Hi")
