44611ef754bceda6da43e422096a245a1718cae6
Martin Nguyen <ngumartin@gmail.com>
2020-01-23 22:37:11 -0800

    index.js minor changes

--- client/assets/javascript/index.js
+++ client/assets/javascript/index.js
@@ -3,6 +3,7 @@ $('document').ready(function () {
     // $("#end-datepicker").datepicker({ dateFormat: 'yy-mm-dd' });
 
     navigator.geolocation.getCurrentPosition(function (position) {
+        console.log(position);
         const lon = position.coords.longitude;
         const lat = position.coords.latitude;
 
@@ -37,16 +38,7 @@ $('document').ready(function () {
         e.preventDefault();
         let city = $("#input-group2").val();
         $("#city").val('');
-        // let startDate = $("#start-datepicker").val().toString();
-        // let endDate = $("#end-datepicker").val().toString();
-        // $("#start-datepicker").val('');
-        // $("#end-datepicker").val('');
-        // if (startDate && startDate != "") {
-        //     startDate += 'T00:00:00Z'
-        // }
-        // if (endDate && endDate != "") {
-        //     endDate += 'T23:59:59Z'
-        // }
+
         console.log(city);
     });
 });
