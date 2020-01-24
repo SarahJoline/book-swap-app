f20e377e3d4f1384a97cc61b3476faa899fa3cba
Jim <jimmybnguyen@yahoo.com>
2020-01-24 14:26:25 -0800

    working on frontend

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize("bookswap_db", "root", "password", {
+const sequelize = new Sequelize("bookswap_db", "root", "Saigon26", {
 
   host: "localhost",
   port: 3306,
