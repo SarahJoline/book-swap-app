8a7e06070b7ef183531bfa159e62536589adcc0e
Martin Nguyen <ngumartin@gmail.com>
2020-01-24 20:47:53 -0800

    minor changes, updated app

--- client/assets/javascript/index.js
+++ client/assets/javascript/index.js
@@ -1,15 +1,11 @@
 $("document").ready(function () {
-  // $("#start-datepicker").datepicker({ dateFormat: 'yy-mm-dd' });
-  // $("#end-datepicker").datepicker({ dateFormat: 'yy-mm-dd' });
-
-
   navigator.geolocation.getCurrentPosition(function (position) {
-
     const lon = position.coords.longitude;
     const lat = position.coords.latitude;
 
 
     console.log(lat, lon);
+    console.log(position);
 
     let apikey = "cc5ff5524ad449ad9cd3d2a5117d86a2";
 
@@ -47,17 +43,7 @@ $("document").ready(function () {
     let city = $("#city").val();
     console.log(city);
     window.location.href = "../../dashboard.html";
-    // $("#city").val("");
-    // let startDate = $("#start-datepicker").val().toString();
-    // let endDate = $("#end-datepicker").val().toString();
-    // $("#start-datepicker").val('');
-    // $("#end-datepicker").val('');
-    // if (startDate && startDate != "") {
-    //     startDate += 'T00:00:00Z'
-    // }
-    // if (endDate && endDate != "") {
-    //     endDate += 'T23:59:59Z'
-    // }
+
   });
 
 });
