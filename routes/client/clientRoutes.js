fe7a2cff93c1894e815cdf70d242693a356dd63f
mariofalvarez <mariofalvarezjr@gmail.com>
2020-01-27 17:47:08 -0800

    minor deleteion of empty code

--- routes/client/clientRoutes.js
+++ routes/client/clientRoutes.js
@@ -3,33 +3,27 @@ const router = express.Router();
 const path = require("path");
 
 router.get("/home", (req, res) => {
-  // ^^ insert file location
-  res.sendFile(path.join(__dirname, "../../client/index.html")); // insert html path
+  res.sendFile(path.join(__dirname, "../../client/index.html"));
 });
 
 router.get("/user/new", (req, res) => {
-  // ^^ insert file location
-  res.sendFile(path.join(__dirname, "../../client/sign-up.html")); // insert html path
+  res.sendFile(path.join(__dirname, "../../client/sign-up.html"));
 });
 
 router.get("/user/profile/new", (req, res) => {
-  // ^^ insert file location
-  res.sendFile(path.join(__dirname, "../../client/profile-set-up.html")); // insert html path
+  res.sendFile(path.join(__dirname, "../../client/profile-set-up.html"));
 });
 
 router.get("/user/profile", (req, res) => {
-  // ^^ insert file location
-  res.sendFile(path.join(__dirname, "../../client/profile.html")); // insert html path
+  res.sendFile(path.join(__dirname, "../../client/profile.html"));
 });
 
 router.get("/booklists", (req, res) => {
-  // ^^ insert file location
-  res.sendFile(path.join(__dirname, "../../client/booklist.html")); // insert html path
+  res.sendFile(path.join(__dirname, "../../client/booklist.html"));
 });
 
 router.get("/dashboard", (req, res) => {
-  // ^^ insert file location
-  res.sendFile(path.join(__dirname, "../../client/dashboard.html")); // insert html path
+  res.sendFile(path.join(__dirname, "../../client/dashboard.html"));
 });
 
 module.exports = router;
